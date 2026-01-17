ef9f6516a345b0b0a1ba81f590fd50ae317efa3c
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2026-01-17 21:33:55 +0700

    buid Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "~/routes";
+import { DefauLayout } from "~/components/Layout";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefauLayout
+            if (route.layout) {
+              Layout = route.layout
+            } else if (route.layout === null) {
+              Layout = Fragment
+            }
+            const Page = route.component
+            return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Search/index.js
@@ -0,0 +1,6 @@
+
+function Search() {
+    return <h2>Search Pages</h2>
+}
+
+export default Search;
