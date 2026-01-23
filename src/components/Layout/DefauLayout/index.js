881cf7c07a6a6aa0d2db24f67500479cecc754b2
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2026-01-23 09:16:40 +0700

    build header + sidebar

--- src/components/Layout/DefauLayout/index.js
+++ src/components/Layout/DefauLayout/index.js
@@ -1,18 +1,22 @@
+import classNames from "classnames/bind";
 import Header from "~/components/Layout/componets/Header";
+import styles from './DefauLayout.module.scss';
 import Sidebar from "./Sidebar";
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx("wrapper")}>
             <Header />
-            <div className="container">
+            <div className={cx("container")}>
                 <Sidebar />
-                <div className="content">
+                <div className={cx("content")}>
                     {children}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default DefaultLayout;
